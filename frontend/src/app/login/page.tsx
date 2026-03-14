cdb975201667ee944326de9c6707577147ba22d3
john <ypoh007@e.ntu.edu.sg>
2026-03-14 14:51:43 +0800

    Added flexbox with flex-1 for element to take up any remaining spaces.

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -13,7 +13,9 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
+    <div className="flex-1 flex flex-col items-center justify-center">
+      <h1 className="text-lg font-bold">Login / Sign up</h1>
+      <img src="/login_img.png" alt="A warm welcoming picture" className="h-1/8 w-1/4" />
       <GoogleAuthButton onClick={loginWithGooglePopUp}/>
     </div>
   );
