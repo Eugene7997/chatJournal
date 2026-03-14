2ff3153437519189c839531032fb7f5d6d8bb44d
john <ypoh007@e.ntu.edu.sg>
2026-03-14 13:46:20 +0800

    Moved Navbar and Footer to layout.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1 className = "flex justify-center align-middle">Hello world</h1>
     </div>
   );
