8e500d791f8abfe14abe75d8d2396d3118383256
john <ypoh007@e.ntu.edu.sg>
2026-03-14 14:13:44 +0800

    Added button for google oauth 2.0 with mock function.

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
+import { useState } from "react";
+import GoogleAuthButton from "@/components/GoogleAuthButton";
+
+
 export default function LoginPage() {
+  const [errors, setErrors] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function loginWithGooglePopUp() {
+    alert("HI");
+  }
+
   return (
     <div>
-      
+      <GoogleAuthButton onClick={loginWithGooglePopUp}/>
     </div>
-  )
+  );
 }
---
+++ frontend/src/components/GoogleAuthButton.tsx
@@ -0,0 +1,22 @@
+import type { GoogleButtonProps } from "@/lib/types/types";
+
+export default function GoogleAuthButton({ onClick, disabled = false }: GoogleButtonProps) {
+
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+            className="w-1/2 bg-white text-black px-4 py-2 mt-2 border border-[#A9A9A9] rounded-lg text-base font-normal cursor-pointer flex items-center justify-center gap-2 transition-colors duration-300 hover:bg-[#EEF5FE] hover:border-[#CBE0FA]"
+        >
+            <img
+                src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                alt="Google Logo"
+                width={20}
+                height={20}
+                className="inline-block"
+            />
+            <span>Continue with Google</span>
+        </button>
+    );
+}
