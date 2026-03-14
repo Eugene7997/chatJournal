52f9a0f0ffa2eb4e4b845283c82303365457a788
john <ypoh007@e.ntu.edu.sg>
2026-03-14 13:45:55 +0800

    Created Footer component.

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="fixed bottom-0 left-0 w-full py-4 flex justify-center z-50">
+            <p>
+                Just a hobby project. Do not upload sensitive information please.
+            </p>
+        </footer>
+    );
+}
