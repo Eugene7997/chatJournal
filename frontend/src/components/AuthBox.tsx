eb8fab5e18e1898b88cbe2429b11233ed0a512e6
john <ypoh007@e.ntu.edu.sg>
2026-03-14 16:13:06 +0800

    AuthBox component for logging in.

---
+++ frontend/src/components/AuthBox.tsx
@@ -0,0 +1,9 @@
+export default function AuthBox() {
+    return (
+        <div>
+            <a href="/auth/login?screen_hint=signup">Signup</a>
+            <br />
+            <a href="/auth/login">Login</a>
+        </div>
+    );
+}
