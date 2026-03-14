6b26171f9a757253d1593c1b9c4f9197b320d66d
john <ypoh007@e.ntu.edu.sg>
2026-03-14 18:45:27 +0800

    Added custom enter behavior for textarea.

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,14 +1,35 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Chat() {
     const [userMsg, setUserMsg] = useState<string>("");
+    const formRef = useRef<HTMLFormElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-    function handleSubmit(e : React.SubmitEvent<HTMLFormElement>) {
+
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
         e.preventDefault()
         alert(userMsg)
     }
+
+    useEffect(() => {
+        const form = formRef.current;
+        const textarea = textareaRef.current;
+        
+        function handleTextAreaCustomEnter(e: KeyboardEvent) {
+            if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault(); // stop newline
+                form?.requestSubmit();
+            }
+        }
+
+        textarea?.addEventListener("keydown", handleTextAreaCustomEnter);
+
+        return () => {
+            textarea?.removeEventListener("keydown", handleTextAreaCustomEnter);
+        }
+    }, []);
 
     return (
         <>
@@ -17,10 +38,12 @@ export default function Chat() {
             </div>
             <footer className="flex-none backdrop-blur-lg border-t border-slate-200">
                 <form
+                    ref={formRef}
                     className="w-full flex gap-8 px-8 py-4"
                     onSubmit={handleSubmit}
                 >
                     <textarea
+                        ref={textareaRef}
                         className="flex-1 border-2 border-gray-300 rounded-xl p-4"
                         placeholder="Start typing!"
                         onChange={(e) => setUserMsg(e.target.value)}
