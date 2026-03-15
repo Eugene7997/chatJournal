9bb60b5751dc3c78112e940a57d4439103fb6a3d
john <ypoh007@e.ntu.edu.sg>
2026-03-15 22:21:45 +0800

    Updated navbar again

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,14 +7,14 @@ export default async function Navbar() {
     return (
         <header>
             <div className="flex items-center justify-between p-4">
-                <img src="/logo.svg" alt="ChatJournal logo" className="h-8 w-8" />
+                <img src="/logo.svg" alt="ChatJournal logo" className="h-12 w-12" />
                 <div className="flex items-center gap-8">
                     <nav className="flex justify-center gap-8">
-                        <a href="/" className="hover:underline">Home</a>
-                        <a href="/about" className="hover:underline">About</a>
-                        {!session && <a href="/login" className="hover:underline">Login or Sign up</a>}
-                        {session && <a href="/chat" className="hover:underline">Chat</a>}
-                        {session && <a href="/account" className="hover:underline">Account</a>}
+                        <a href="/" className="text-xl hover:underline">Home</a>
+                        <a href="/about" className="text-xl hover:underline">About</a>
+                        {!session && <a href="/login" className="text-xl hover:underline">Login or Sign up</a>}
+                        {session && <a href="/chat" className="text-xl hover:underline">Chat</a>}
+                        {session && <a href="/account" className="text-xl hover:underline">Account</a>}
                     </nav>
                     {session?.user?.picture && <NavbarProfile src={session.user.picture} />}
                 </div>
