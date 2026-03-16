75ac9f737847a65cc966c8eabacc5d4a24507dc1
john <ypoh007@e.ntu.edu.sg>
2026-03-16 20:42:28 +0800

    Fixed up Footer always at bottom issue.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 w-full py-4 flex justify-center z-50">
+        <footer className="bottom-0 left-0 w-full py-4 flex justify-center z-50">
             <p>
                 Just a hobby project. Do not upload sensitive information please.
             </p>
