4f5d3cf848319fae17e4e901319762e2a553e17f
john <ypoh007@e.ntu.edu.sg>
2026-04-02 20:48:54 +0800

    changed anchor tags to nextJS <Link>

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { auth0 } from "@/lib/auth/auth0";
 import NavbarProfile from "@/components/NavbarProfile";
 
@@ -11,12 +12,12 @@ export default async function Navbar() {
                 <Image width={100} height={100} src="/logo.svg" alt="ChatJournal logo" className="h-12 w-12" />
                 <div className="flex items-center gap-8">
                     <nav className="flex justify-center gap-8">
-                        <a href="/" className="text-xl hover:underline">Home</a>
-                        <a href="/about" className="text-xl hover:underline">About</a>
-                        {!session && <a href="/login" className="text-xl hover:underline">Login or Sign up</a>}
-                        {session && <a href="/chat" className="text-xl hover:underline">Chat</a>}
-                        {session && <a href="/journals" className="text-xl hover:underline">Journals</a>}
-                        {session && <a href="/account" className="text-xl hover:underline">Account</a>}
+                        <Link href="/" className="text-xl hover:underline">Home</Link>
+                        <Link href="/about" className="text-xl hover:underline">About</Link>
+                        {!session && <Link href="/login" className="text-xl hover:underline">Login or Sign up</Link>}
+                        {session && <Link href="/chat" className="text-xl hover:underline">Chat</Link>}
+                        {session && <Link href="/journals" className="text-xl hover:underline">Journals</Link>}
+                        {session && <Link href="/account" className="text-xl hover:underline">Account</Link>}
                     </nav>
                     {session?.user?.picture && <NavbarProfile src={session.user.picture} />}
                 </div>
