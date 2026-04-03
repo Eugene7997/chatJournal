863c27d68c767a41af8adede68b9e1653065dc98
john <ypoh007@e.ntu.edu.sg>
2026-04-03 13:12:50 +0800

    Modified Footer Component for mobile UI

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bottom-0 left-0 w-full py-4 flex justify-center z-50">
-            <p>
+        <footer className="bottom-0 left-0 w-full px-4 py-4 flex justify-center z-50">
+            <p className="text-xs sm:text-sm text-center opacity-60">
                 Just a hobby project. Do not upload sensitive information please.
             </p>
         </footer>
