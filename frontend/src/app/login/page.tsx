3837b95f06192a2899934f43c0e4b43ed13fd0c4
john <ypoh007@e.ntu.edu.sg>
2026-04-03 13:12:26 +0800

    Added mobile UI for login page but also replaced img with Image.

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { auth0 } from "@/lib/auth/auth0";
 import AuthBox from "@/components/AuthBox";
 
@@ -8,7 +9,7 @@ export default async function LoginPage() {
     return (
       <div className="flex-1 flex flex-col items-center justify-center">
         <h1 className="text-lg font-bold">Login / Sign up</h1>
-        <img src="/login_img.png" alt="A warm welcoming picture" className="h-1/8 w-1/4" />
+        <Image src="/login_img.png" alt="A warm welcoming picture" width={100} height={100} className="w-1/2 sm:w-1/4 max-w-xs h-auto my-4" />
         <AuthBox />
       </div>
     );
