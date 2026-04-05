dd6e21ca34edc6fc3ad9f3cd5b46e5851bd1bc8e
john <ypoh007@e.ntu.edu.sg>
2026-04-05 12:01:06 +0800

    Fixed incorrect env variables referencing issue.
    I should probably just delete this file. But I may use it down the road.

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,9 +1,2 @@
 import '@testing-library/jest-dom'
 
-// Provide dummy env vars so API route modules load without errors in tests
-process.env.OPENROUTER_API_KEY = 'test-openrouter-key'
-process.env.AUTH0_M2M_CLIENT_ID = 'test-m2m-client-id'
-process.env.AUTH0_M2M_CLIENT_SECRET = 'test-m2m-secret'
-process.env.AUTH0_M2M_DOMAIN = 'test.auth0.com'
-process.env.AUTH0_DOMAIN = 'test.auth0.com'
-process.env.DB_URL = 'postgresql://test'
